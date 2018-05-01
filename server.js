7828e5a75a39b16b85f03d8e988625811610904b
Philippe Recto <precto1285@gmal.com>
2018-04-30 20:49:59 -0700

    added server.js

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 var express = require('express');
 var mysql = require('mysql');
+
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
 var exphbs = require('express-handlebars');
 
+app.engine("handlebars", exphbs({ defaultlayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function(){
+    console.log("Server listening on: https//localhost:" + PORT);
+});
