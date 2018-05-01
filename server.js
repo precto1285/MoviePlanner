7125f07c9000a9f594fef32180a58320132c4592
Philippe Recto <precto1285@gmal.com>
2018-04-30 21:03:17 -0700

    added to server.js

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var mysql = require('mysql');
-
 var bodyParser = require('body-parser');
 
 var app = express();
@@ -14,6 +12,44 @@ var exphbs = require('express-handlebars');
 
 app.engine("handlebars", exphbs({ defaultlayout: "main"}));
 app.set("view engine", "handlebars");
+
+
+app.get("/", function(req, res){
+
+});
+
+app.post("/movies", function(req, res){
+
+});
+
+app.put("/movies", function(req, res){
+
+});
+
+app.delete("/movies", function(req, res){
+
+});
+
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'P!n0yR0ck*',
+    database: 'moviePlannerDB'
+});  
+
+connection.connect(function(err){
+    if(err){
+        console.log("error connecting: " + err.stack);
+        return;
+    }
+
+    console.log("Connect as id: " + connection.threadId);
+});
+
+
 
 app.listen(PORT, function(){
     console.log("Server listening on: https//localhost:" + PORT);
